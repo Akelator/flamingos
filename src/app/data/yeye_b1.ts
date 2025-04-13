e337f1e2f645535ea56e767a430959b6072de491
Adrian <adrianriera@gmail.com>
2025-04-13 20:45:40 +0200

    po ya esta

--- src/app/data/yeye_b1.ts
+++ src/app/data/yeye_b1.ts
@@ -3,10 +3,87 @@ import { CUT } from './model';
 export const y1: CUT[] = [
   {
     T: '0:00:00',
-    song: 'Toy trabajando en ello',
-    tempo: 100,
+    song: 'Libre',
+    tempo: 96,
   },
   {
-    T: '20:00:00',
+    T: '2:28:62',
+  },
+  {
+    T: '2:50:19',
+    song: 'Chica Yeye',
+    tempo: 96,
+  },
+  {
+    T: '4:39:18',
+  },
+  {
+    T: '4:51:57',
+    song: 'Que Se Muere Los Feos',
+    tempo: 80,
+  },
+  {
+    T: '6:18:62',
+  },
+  {
+    T: '6:56:21',
+    song: 'Help Ayudame',
+    tempo: 130,
+  },
+  {
+    T: '8:33:09',
+  },
+  {
+    T: '8:43:25',
+    song: 'Eva Maria',
+    tempo: 130,
+  },
+  {
+    T: '10:15:95',
+  },
+  {
+    T: '10:30:38',
+    song: 'Corazon Contento',
+    tempo: 130,
+  },
+  {
+    T: '11:50:64',
+  },
+  {
+    T: '12:06:77',
+    song: 'Mi Gran Noche',
+    tempo: 135,
+  },
+  {
+    T: '13:35:50',
+  },
+  {
+    T: '13:53:32',
+    song: 'Saca el Guisky Cheli',
+    tempo: 135,
+  },
+  {
+    T: '15:20:38',
+  },
+  {
+    T: '15:37:95',
+    song: 'Este Amor No Se Toca',
+    tempo: 140,
+  },
+  {
+    T: '17:14:80',
+  },
+  {
+    T: '17:42:09',
+    song: 'Black Is Black',
+    tempo: 130,
+  },
+  {
+    T: '19:51:70',
+  },
+  {
+    T: '19:54:64',
+    song: 'FINAL',
+    tempo: 130,
   },
 ];
--- src/app/data/yeye_b2.ts
+++ src/app/data/yeye_b2.ts
@@ -2,11 +2,83 @@ import { CUT } from './model';
 
 export const y2: CUT[] = [
   {
-    T: '0:00:00',
-    song: 'Toy trabajando en ello',
-    tempo: 100,
+    T: '00:00:00',
+    song: 'Quisiera Ser',
+    tempo: 90,
   },
   {
-    T: '20:00:00',
+    T: '2:12:83',
+  },
+  {
+    T: '2:25:85',
+    song: 'La Fiesta De Blas',
+    tempo: 96,
+  },
+  {
+    T: '3:35:56',
+  },
+  {
+    T: '3:47:97',
+    song: 'Porque Te Vas',
+    tempo: 96,
+  },
+  {
+    T: '5:35:52',
+  },
+  {
+    T: '5:58:00',
+    song: 'Resistire',
+    tempo: 96,
+  },
+  {
+    T: '8:37:98',
+  },
+  {
+    T: '8:57:94',
+    song: 'Un Beso Y Una Flor',
+    tempo: 96,
+  },
+  {
+    T: '11:07:45',
+  },
+  {
+    T: '11:27:43',
+    song: 'Puente A Mallorca',
+    tempo: 96,
+  },
+  {
+    T: '13:25:11',
+  },
+  {
+    T: '13:26:43',
+    song: 'Juntos',
+    tempo: 135,
+  },
+  {
+    T: '15:38:97',
+  },
+  {
+    T: '15:57:65',
+    song: 'Oh Oh July',
+    tempo: 135,
+  },
+  {
+    T: '17:42:04',
+  },
+  {
+    T: '17:47:99',
+    song: 'Un Rallo De Sol',
+    tempo: 135,
+  },
+  {
+    T: '19:13:76',
+  },
+  {
+    T: '19:14:85',
+    song: 'El Partido De Futbol ',
+    tempo: 130,
+  },
+  {
+    T: '20:41:15',
   },
 ];
